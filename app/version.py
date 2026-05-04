54503d8e4dd894ae22a0c787d6a8eceb65cfb067
BigTonyTones <shamelinc@gmail.com>
2026-05-04 08:09:17 -0700

    Bump version to v7.1.5

--- app/version.py
+++ app/version.py
@@ -3,7 +3,7 @@ Version management for Tonys Onvif-RTSP Server
 Single source of truth for version information
 """
 
-CURRENT_VERSION = "7.1.3"
+CURRENT_VERSION = "7.1.5"
 
 def parse_version(version_str):
     """
